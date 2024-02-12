ba6ab8eb8d9b79d190ee4c9e0f8db892b61460ad
Jerry Boonstra <jerry@obexmetrics.com>
2024-02-11 19:57:19 -0800

    add a way to test image_uri for sklearn docker container

---
+++ program/get_framework_uri.py
@@ -0,0 +1,19 @@
+from sagemaker.image_uris import retrieve
+
+# Define your configuration for us-east-2 region and ml.m5.xlarge instance type
+config = {
+    "session": {
+        "boto_region_name": "us-east-2"  # Updated to us-east-2 region
+    },
+    "instance_type": "ml.m5.xlarge"  # Updated to ml.m5.xlarge instance type
+}
+
+# Call the retrieve function for scikit-learn framework in us-east-2 region
+framework_uri = retrieve(
+    framework='sklearn',  # Use 'sklearn' for scikit-learn
+    version='0.23-1',  # Specify the scikit-learn version you want to use
+    region=config["session"]["boto_region_name"]  # This will use "us-east-2"
+)
+
+print(f"Retrieved framework_uri for us-east-2: {framework_uri}")
+
